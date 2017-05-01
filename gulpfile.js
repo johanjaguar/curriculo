58496dc90410ddce066be4d578f5cedda72b1983
johanjaguar <wormwoodster@gmail.com>
2017-05-01 13:28:03 -0500

    primeros pasos para integracion de la cv con gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+//variables con los plugins que usaremos
+var gulp = require('gulp');
+var util = require('gulp-util');
+var requireDir = require('require-dir');
+var browserSync = require('browser-sync');
+requireDir('./gulp-tasks');
+
+//tarea default-watch
+gulp.task( 'default', ['browser-sync'], function(){
+//accion que ejecuta y vigila el directorio con la tarea asociada
+
+gulp.watch('sources/scss/**/*.scss', ['sass','cssmin']);
+//cssgulp.watch('sources/haml/**/*.haml', ['haml']);
+//gulp.watch('sources/javascript/**/*.js', ['js']);
+//gulp.watch([
+//'sources/images/**/*.gif', 'sources/images/**/*.png', 'sources/images/**/*.jpg', 'sources/images/**/*.jpeg']
+//,['imagesmin']
+//);
+gulp.watch("**/*.html").on('change', browserSync.reload);
+}
+
+);
+
+
+//BROWSERSYNC
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+// Static server.
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
+});
