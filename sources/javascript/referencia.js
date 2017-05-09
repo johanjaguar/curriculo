5653e835e2ddadf648750fbad1b522a5d1e5fdc0
johanjaguar <wormwoodster@gmail.com>
2017-05-09 00:19:28 -0500

    inicio de traduccion inclsion de tag lang y referencia js adaptado a multiidioma

--- sources/javascript/referencia.js
+++ sources/javascript/referencia.js
@@ -1,16 +1,21 @@
 function referencia(link, imagen, empresa, cargo, telefono, anio, jefe){
-    anio = anio || " ";
-    jefe = jefe || " ";
     var html = '';
+    var lang = document.documentElement.lang;
 		html += '<div class="workItem">';
 		html += '	<a href="'+link+'" target="_blank">';
 		html += '		<p class="wiImgEmpresa"><img src="build/images-op/'+imagen+'.png"/></p>';
 								if( empresa.length > 0 ){ html += '		<p class="wiEmpresa">'+empresa+'</p>'; }
 								if( cargo.length > 0 ){ html += '		<p class="wiCargo">'+cargo+'</p>';}
 								if( anio.length > 0 ){ html += '		<p class="wiAnio">'+anio+'</p>'; }
-								if( jefe.length > 0 ){ html += '		<p class="wiJefe">Jefe Inmediato:<br/>'+jefe+'</p>';}
+								if( jefe != 'NULL' ){
+                  if(lang == "en")
+                    html += '		<p class="wiJefe">Boss:<br/>'+jefe+'</p>';
+                  else
+                    html += '		<p class="wiJefe">Jefe Inmediato:<br/>'+jefe+'</p>';
+                }
 								if( telefono.length > 0 ){ html += '		<p class="wiTelefono">'+telefono+'</p>';}
 		html += '	</a>';
 		html += '</div>';
+
     return html;
 }
