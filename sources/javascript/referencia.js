43db1853db3e904723b43c858a0a4f22bca49698
petvar <petvar@eltiempo.com>
2017-05-10 16:13:02 -0500

    actualizacion de pagina web en español y pequeños fixes

--- sources/javascript/referencia.js
+++ sources/javascript/referencia.js
@@ -6,12 +6,14 @@ function referencia(link, imagen, empresa, cargo, telefono, anio, jefe){
 		html += '		<p class="wiImgEmpresa"><img src="build/images-op/'+imagen+'.png"/></p>';
 								if( empresa.length > 0 ){ html += '		<p class="wiEmpresa">'+empresa+'</p>'; }
 								if( cargo.length > 0 ){ html += '		<p class="wiCargo">'+cargo+'</p>';}
-								if( anio.length > 0 ){ html += '		<p class="wiAnio">'+anio+'</p>'; }
+								if( anio != 'NULL' ){
+                  html += '<p class="wiAnio">'+ anio +'</p>';
+                }
 								if( jefe != 'NULL' ){
                   if(lang == "en")
-                    html += '		<p class="wiJefe">Inmediate Supervisor:<br/>'+jefe+'</p>';
+                    html += '<p class="wiJefe">Inmediate Supervisor:<br/>'+jefe+'</p>';
                   else
-                    html += '		<p class="wiJefe">Jefe Inmediato:<br/>'+jefe+'</p>';
+                    html += '<p class="wiJefe">Jefe Inmediato:<br/>'+jefe+'</p>';
                 }
 								if( telefono.length > 0 ){ html += '		<p class="wiTelefono">'+telefono+'</p>';}
 		html += '	</a>';
