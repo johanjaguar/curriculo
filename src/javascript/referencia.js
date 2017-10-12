85722b4e47a6fbd1e665599cdba016201829192b
Johan Vargas <johanvargas@outlook.com>
2017-10-12 01:19:14 -0500

    sass temas y modificaciones a js, temas monokai y mono incluidos

--- src/javascript/referencia.js
+++ src/javascript/referencia.js
@@ -1,23 +1,29 @@
-function referencia(link, imagen, empresa, cargo, telefono, anio, jefe){
-    var html = '';
-    var lang = document.documentElement.lang;
-		html += '<div class="workItem">';
-		html += '	<a href="'+link+'" target="_blank">';
-		html += '		<p class="wiImgEmpresa"><img src="build/images-op/'+imagen+'.png"/></p>';
-								if( empresa.length > 0 ){ html += '		<p class="wiEmpresa">'+empresa+'</p>'; }
-								if( cargo.length > 0 ){ html += '		<p class="wiCargo">'+cargo+'</p>';}
-								if( anio != 'NULL' ){
-                  html += '<p class="wiAnio">'+ anio +'</p>';
-                }
-								if( jefe != 'NULL' ){
-                  if(lang == "en")
-                    html += '<p class="wiJefe">Inmediate Supervisor:<br/>'+jefe+'</p>';
-                  else
-                    html += '<p class="wiJefe">Jefe Inmediato:<br/>'+jefe+'</p>';
-                }
-								if( telefono.length > 0 ){ html += '		<p class="wiTelefono">'+telefono+'</p>';}
-		html += '	</a>';
-		html += '</div>';
+function referencia(link, imagen, empresa, cargo, telefono, anio, jefe) {
+	var html = '';
+	var lang = document.documentElement.lang;
+	html += '<div class="workItem">';
+	html += '	<a href="' + link + '" target="_blank">';
+	html += '		<p class="wiImgEmpresa"><img src="build/images-op/' + imagen + '.png"/></p>';
+	if (empresa.length > 0) {
+		html += '		<p class="wiEmpresa">' + empresa + '</p>';
+	}
+	if (cargo.length > 0) {
+		html += '		<p class="wiCargo">' + cargo + '</p>';
+	}
+	if (anio != 'NULL') {
+		html += '<p class="wiAnio">' + anio + '</p>';
+	}
+	if (jefe != 'NULL') {
+		if (lang == "en")
+			html += '<p class="wiJefe">Inmediate Supervisor:<br/>' + jefe + '</p>';
+		else
+			html += '<p class="wiJefe">Jefe Inmediato:<br/>' + jefe + '</p>';
+	}
+	if (telefono.length > 0) {
+		html += '		<p class="wiTelefono">' + telefono + '</p>';
+	}
+	html += '	</a>';
+	html += '</div>';
 
-    return html;
+	return html;
 }
---
+++ src/javascript/temas.js
@@ -0,0 +1,22 @@
+/*
+var bodyTag = document.getElementsByTagName("body");
+var tema = document.getElementsByClassName("tema_li");
+
+var cambiaTema = function () {
+	console.log('cambiatema test 2 3 4 5');
+	var theme = this.getAttribute("data-theme");
+ 	bodyTag.className = theme;
+
+};
+
+for (var i = 0; i < tema.length; i++) {
+	tema[i].addEventListener('click', cambiaTema, false);
+}
+*/
+
+
+/*for (var i = 0; i < tema.length; i++) {
+	tema[i].addEventListener('click', function(event) {
+		console.log('cambiatema test 2 3 4 5');
+	});
+}*/
