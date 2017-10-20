d08af844c65364f118cd85cea0386e2e8f8496d4
petvar <petvar@eltiempo.com>
2017-10-20 14:55:13 -0500

    mejora en javascript para llenar la informacion por json adicion de objeto links y mejora con las traducciones (incompleto)

--- src/javascript/getData.js
+++ src/javascript/getData.js
@@ -1,41 +1,69 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'data/textos.json', true);
+// listen for the DOMContentLoaded event, then bind our function
+document.addEventListener('DOMContentLoaded', function () {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'data/textos.json', true);
 
-request.onload = function () {
-	if (request.status >= 200 && request.status < 400) {
-		// Success!
-		var data = JSON.parse(request.responseText);
-		setText(".notaBox p", data.textos.spanish.notaBox);
-		setThemeList(data.temas);
+	request.onload = function () {
+		if (request.status >= 200 && request.status < 400) {
+			// Success!
+			var data = JSON.parse(request.responseText);
+			var textos = "";
+			var languaje = document.documentElement.lang;
 
-	} else {
-		// We reached our target server, but it returned an error
+			if (languaje == "en") {
+				textos = data.textos.english;
+			} else {
+				textos = data.textos.spanish;
+			}
 
+			var links = data.links;
+
+			fillHTML(textos);
+			fillLinks(links);
+			setThemeList(data.temas);
+
+		} else {
+			// We reached our target server, but it returned an error
+
+		}
+	};
+
+	request.onerror = function () {
+		// There was a connection error of some sort
+	};
+
+	request.send();
+});
+
+function fillHTML(textos) {
+	for (var key in textos) {
+		if (textos.hasOwnProperty(key)) {
+			setText("." + key, textos[key]);
+		}
 	}
-};
+}
 
-request.onerror = function () {
-	// There was a connection error of some sort
-};
-
-request.send();
-
+function fillLinks(links) {
+	for (var key in links) {
+		if (links.hasOwnProperty(key)) {
+			setLink("#link_" + key, links[key]);
+		}
+	}
+}
 
 function setText(selector, texto) {
 	let elem = document.querySelector(selector);
 	elem.innerHTML = elem.innerHTML + texto;
 }
 
+function setLink(selector, link) {
+	let elem = document.querySelector(selector);
+	elem.href = link;
+}
 
 function setThemeList(themes) {
 	let elem = document.querySelector('.tema');
 	var length = themes.length;
-
-	/*
-	for (var i = 0; i < length; i++) {
-		elem.innerHTML = elem.innerHTML + "<li class='tema_li' data-theme='" + themes[i].valor + "'>" + themes[i].nombre + "</li>";
-		console.log("tema actual" + themes[i]);
-	}*/
 
 	Object.keys(themes).forEach(function (key) {
 		elem.innerHTML = elem.innerHTML + "<li class='tema_li' data-theme='" + themes[key].valor + "'>" + themes[key].nombre + "</li>";
