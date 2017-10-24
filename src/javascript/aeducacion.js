201a13d491886b916b9d8144f83cecf9588297a2
Johan Vargas <johanvargas@outlook.com>
2017-10-23 21:11:16 -0500

    tag actual js modular, quitar jquery

--- src/javascript/aeducacion.js
+++ src/javascript/aeducacion.js
@@ -1,3 +1,19 @@
+function setEducacion(instituciones, selector) {
+	let elem = document.querySelector(selector);
+	elem.innerHTML = "";
+
+	for (var key in instituciones) {
+		var ref = instituciones[key];
+		var languaje = document.documentElement.lang;
+
+		if (languaje == "en") {
+			elem.innerHTML = elem.innerHTML + educacion(ref.icono, ref.fecha, ref.nombre, ref.cursoEng);
+		} else {
+			elem.innerHTML = elem.innerHTML + educacion(ref.icono, ref.fecha, ref.nombre, ref.curso);
+		}
+	}
+}
+
 function educacion(icono, anio, institucion, carrera) {
 	var html = '';
 	html += '<div class="educationItem">';
